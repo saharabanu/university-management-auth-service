c8a9d66d4a90d280d706dc31c0c6d147a00975ae
saharabanu <saharabanungn@gmail.com>
2023-06-01 11:33:14 +0600

    user interface and model done

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,6 @@ import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 
-
-
-
 // middleware
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
