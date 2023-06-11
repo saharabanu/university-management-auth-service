7e0fb8cf5c0565c4dd6fb68a68b68d8913ea9361
saharabanu <saharabanungn@gmail.com>
2023-06-11 11:53:07 +0600

    academic semester constant done

---
+++ src/app/modules/academicSemister/academicSemester.constant.ts
@@ -0,0 +1,31 @@
+import {
+  IAcademicSemesterCodes,
+  IAcademicSemesterMonth,
+  IAcademicSemesterTitles,
+} from './academicSemister.interface';
+
+export const academicSemesterMonths: IAcademicSemesterMonth[] = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const academicSemesterCodes: IAcademicSemesterCodes[] = [
+  '01',
+  '02',
+  '03',
+];
+export const academicSemesterTitles: IAcademicSemesterTitles[] = [
+  'Autumn',
+  'Summer',
+  'Fall',
+];
