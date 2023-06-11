09d1e6b0ff61f23080a64b549f0a9d1dc4a83915
saharabanu <saharabanungn@gmail.com>
2023-06-11 14:56:55 +0600

    [DONE] handle same year, same semester validation

--- src/app/modules/academicSemister/academicSemester.model.ts
+++ src/app/modules/academicSemister/academicSemester.model.ts
@@ -3,11 +3,14 @@ import {
   AcademicSemesterModel,
   IAcademicSemester,
 } from './academicSemister.interface';
+
 import {
   academicSemesterCodes,
   academicSemesterMonths,
   academicSemesterTitles,
 } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+import status from 'http-status';
 
 const academicSemesterSchema = new Schema<IAcademicSemester>(
   {
@@ -40,6 +43,19 @@ const academicSemesterSchema = new Schema<IAcademicSemester>(
     timestamps: true,
   }
 );
+
+// for checking and control same same semester
+academicSemesterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemester.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(status.CONFLICT, 'Academic Semester is already exist !');
+  }
+  next();
+});
 
 export const AcademicSemester = model<IAcademicSemester, AcademicSemesterModel>(
   'AcademicSemester',
